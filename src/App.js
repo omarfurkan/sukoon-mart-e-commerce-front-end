efa0d9dd6b2e9e50f07196f78fbab83f445d51d6
omarfurkan <omarfurkan92@gmail.com>
2023-07-10 11:31:53 +0600

    partial design of ProductList with tailwind components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ProductList from './features/product-list/ProductList';
 
 
 function App() {
   return (
     <div className="App">
-
+      <ProductList />
     </div>
   );
 }
